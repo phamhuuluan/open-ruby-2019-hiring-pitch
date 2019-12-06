c4df509f70fee9281bd5eee817e832b10884aec9
phamhuuluan <luanphamm15@gmail.com>
2019-12-06 14:15:10 -0500

    Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,24 @@ class UsersController < ApplicationController
     @user = User.new 
   end 
 
-  def create
+  def create 
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       redirect_to root_path
     else
       flash[:danger] =  t ".error"
       render :new
     end
+  end
+  
+  def show 
+    @user = User.find_by id: params[:id]
+      
+    return if @user 
+    flash[:danger] =  t ".no_user"
+    redirect_to root_url
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root to: "static_pages#home"
   
   get "static_pages/home"
-  get  "/signup",  to: "users#new"
-  post "/signup",  to: "users#create"
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
-  resources :users, only: %i(new create)
+  resources :users, only: %i(new create show)
 
 end
